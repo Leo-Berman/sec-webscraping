19e3591761d95aed6c3cb7be0850bd33b4a0a647
leo-berman <leo.berman@temple.edu>
2024-03-14 15:41:12 -0400

    established venv and started working on a script to scrape all the links

---
+++ 1414932/scraping_links/scrape_links.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as bs
+import os
+import pandas as pd
+import requests
+import time
+def parse_filings():
+    filings_path = os.path.abspath("filings_links.tsv")
+    df = pd.read_csv(filings_path, sep='\t',header = 0)
+    df = df.drop(["Form type","Form description","Filing date"],axis = "columns")
+    
+    '''
+    Iterate through all links temporarily disabled
+    for i in df.index:
+        open_link(df['Filings URL'][i])
+    '''
+    open_link(df['Filings URL'][0])
+
+def open_link(input_URL):
+    headers = {
+        "User-Agent": "Temple University leo.berman@temple.edu",
+        "Accept-Encoding": "gzip, deflate",
+        "Host": "www.sec.gov"
+    }
+    base = requests.get(input_URL,headers)
+    soup = bs(base.content, "html.parser")
+    print(soup.prettify())
+    time.sleep(10)
+    pass
+def main():
+    parse_filings()
+    pass
+
+if __name__ == "__main__":
+    main()
