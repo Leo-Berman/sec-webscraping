5880cfac4215bd1a271f06b1eaacd26721e83832
Leo Berman <lberman@malvern.ezsoft-inc.com>
2024-06-22 00:08:19 -0400

    code refactor

--- BDCGeneral.py
+++
@@ -1,7 +0,0 @@
-import process_table as pt
-
-async def main():
-    pass
-
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
---
+++ General/browser_interactions.py
@@ -0,0 +1,93 @@
+import asyncio
+import pyppeteer as pt
+import unicodedata
+import pandas as pd
+import pdb
+
+import process_table as ptab
+
+async def initialize_browser(link):
+    browser = await pt.launch({"headless" : False})
+    page = await browser.newPage()
+
+    await page.goto(link,{"waitUntil" : ['domcontentloaded',
+                    'networkidle2','networkidle0'] })
+
+    return page,browser
+
+async def find_frame(page):
+
+    for frame in page.frames:
+        if frame.name == "ixvFrame":
+            return frame
+
+async def narrow_divs(divs,date):
+
+    for index,div in enumerate(divs):
+        content = await div.getProperty("textContent")
+        text = unicodedata.normalize('NFKC',content.toString()).replace("JSHandle:","")
+
+        if text.__contains__("Consolidated Schedule of Investments"):
+            
+            content = await divs[index+1].getProperty("textContent")
+            text =  unicodedata.normalize('NFKC',content.toString()).replace("JSHandle:","")
+
+            content = await  divs[index+2].getProperty("textContent")
+            text +=  unicodedata.normalize('NFKC',content.toString()).replace("JSHandle:","")
+
+            content = await  divs[index+3].getProperty("textContent")
+            text +=  unicodedata.normalize('NFKC',content.toString()).replace("JSHandle:","")
+
+            if (text.__contains__(date[0]) and
+                text.__contains__(date[1]) and
+                text.__contains__(date[2])):
+                return divs[index::]
+    return []
+        
+async def make_table(page,date):
+
+    targets = []
+    
+    try:
+        frame = await find_frame(page)
+        breakpoint()
+        target_frame_content = await frame.querySelector('html > body > div.reboot.main-container > #dynamic-xbrl-form')
+        breakpoint()
+        divs = await target_frame_content.querySelectorAll('div');
+        breakpoint()
+        targets = await narrow_divs(divs,date)
+        breakpoint()
+        
+        
+    except:
+        print("Frame not found")
+
+    if len(targets) == 0:
+
+        try:
+            query = "html > body > *"
+            doc = await page.querySelectorAll(query)
+            targets.extend(doc)
+
+            while doc:
+                targets.extend(doc)
+                query +=" > *"
+                doc = await page.querySelectorAll(query)
+        except:
+            print("Date filing not found",)
+    df = pd.DataFrame()
+
+    for target in targets:
+        
+        table = await target.querySelector('table > tbody')
+        
+        if table:
+
+            return_rows,continue_bool = await ptab.process_table(table)
+            
+            df = pd.concat([df,return_rows])
+            if continue_bool == False:
+                print("is this found?")
+                break
+
+    return df
